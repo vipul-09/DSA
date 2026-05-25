70ed06dd18b4abc660bff8d5f41c66db54049db3
Vipul Kumar Verma <vipulkverma1@gmail.com>
2026-05-25 15:43:13 +0000

    Sync LeetCode submission Runtime - 16 ms (10.44%), Memory - 56.5 MB (10.57%)

---
+++ 2001-jump-game-vii/solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean canReach(String s, int minJump, int maxJump) {
+
+        int n = s.length();
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+
+        queue.offer(0);
+        visited[0] = true;
+
+        int farthest = 0;
+
+        while (!queue.isEmpty()) {
+
+            int i = queue.poll();
+
+            // reached end
+            if (i == n - 1) {
+                return true;
+            }
+
+            int start = Math.max(i + minJump, farthest + 1);
+            int end = Math.min(i + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+
+                if (s.charAt(j) == '0' && !visited[j]) {
+
+                    visited[j] = true;
+                    queue.offer(j);
+                }
+            }
+
+            farthest = end;
+        }
+
+        return false;
+    }
+}
