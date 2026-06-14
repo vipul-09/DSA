675fcd7104f30266fa66381876d2b70afd114916
Vipul Kumar Verma <vipulkverma1@gmail.com>
2026-06-14 17:05:41 +0000

    Sync LeetCode submission Runtime - 2 ms (93.94%), Memory - 72.1 MB (40.89%)

---
+++ 0238-product-of-array-except-self/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+
+        // Left products
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+
+        // Right products
+        for (int i = n - 1; i >= 0; i--) {
+
+            answer[i] *= rightProduct;
+
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+}
